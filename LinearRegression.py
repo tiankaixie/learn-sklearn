071aa216b8830a882b2193cd619bcde92cb73b5d
Tiankai Xie <tiankaixie@gmail.com>
2017-06-15 16:09:26 -0700

    Linear Regression part 3
    Linear Regression part 3: basically added one column for forecasting.
    Guess ‘df’ is one of objects that quandl provided. So get the forecast
    of couple of days in the future (0.01 * len means few days) and shift
    (don’t know what that is) will see in the next chap.

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import quandl
+import quandl, math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -10,4 +10,13 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df ['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df ['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-print(df.head())
+#print(df.head())
+
+forecast_col = 'Adj. Close'
+
+df.fillna(-99999, inplace = True)
+
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace = True)
+print(df.head()) 
