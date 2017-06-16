3a1baa44f3d2f2b3e547cd5587f348367c88e86e
Tiankai Xie <tiankaixie@gmail.com>
2017-06-15 17:28:58 -0700

    Linear Regression part 5
    Linear Regression part 5: the basic idea is that we have our model
    already, and we use predict to predict the future’s stock price because
    we got good accuracy. the we use plot tools to plot.

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,8 +1,12 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use("ggplot")
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -22,13 +26,16 @@ df.fillna(-99999, inplace = True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace = True)
+
 #print(df.head()) 
 
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out:]
+
+df.dropna(inplace = True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
@@ -37,4 +44,27 @@ clf = LinearRegression() #n_jobs = 1
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+# print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_day
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df ['Adj. Close'].plot()
+df ['Forecast'].plot()
+plt.legend(loc = 4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
